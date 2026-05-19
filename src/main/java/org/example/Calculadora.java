e1ef7a4f5b00e631f19d0445cf370b7e78e9c83a
Victor Flores <20253ds136@utez.edu.mx>
2026-05-19 13:41:43 -0600

    [NEW] metodo multiplicar

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -14,8 +14,8 @@ public class Calculadora {
         return numero1 + numero2;
     }
 
-    public double restar() {
-        return numero1 - numero2;
+    public double multiplicar() {
+        return numero1 * numero2;
     }
 
 }
